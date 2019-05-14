cd34d7629fda13d92bb6ecdac9a52b820db958b8
Roy's <leroypakade@gmail.com>
2019-05-15 00:48:42 +0200

    MSMQ with WebAPI

---
+++ MessageAPI/Controllers/MessageController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Experimental.System.Messaging;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MessageAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        public static List<string> UserRequest = new List<string>();
+
+        [HttpPost("UserSignup")]
+        public void UserSignup([FromBody]User payLoad)
+        {
+            try
+            {
+                MessageQueue mq = GetQueueReference();
+                mq.Formatter = new XmlMessageFormatter(new[] { typeof(User) });
+
+                // Add an event handler for the PeekCompleted event.
+                mq.PeekCompleted += OnPeekCompleted;
+
+                // Begin the asynchronous peek operation.
+                mq.BeginPeek();
+
+                SendOneMessage(payLoad);
+
+                Console.WriteLine("Listening on queue");
+
+                Console.ReadLine();
+
+                Console.WriteLine("Closing listener...");
+
+                // Remove the event handler before closing the queue
+                mq.PeekCompleted -= OnPeekCompleted;
+                mq.Close();
+                mq.Dispose();
+            }
+            catch(Exception ex )
+            {
+
+                throw ex;
+            }
+        }
+
+        [HttpGet("UserSignupResponse")]
+        public List<string> UserSignupResponse()
+        {
+            return UserRequest;
+          
+        }
+
+        private static MessageQueue GetQueueReference()
+        {
+            const string queueName = @".\private$\testqueue";
+            MessageQueue queue;
+            if (!MessageQueue.Exists(queueName))
+                queue = MessageQueue.Create(queueName, true);
+            else
+                queue = new MessageQueue(queueName);
+            return queue;
+        }
+
+        async void SendOneMessage(User payLoad)
+        {
+            int _messageNum = 0;
+            // Create a transaction because we are using a transactional queue.
+            using (var trn = new MessageQueueTransaction())
+            {
+                try
+                {
+                    // Create queue object
+                    using (var queue = GetQueueReference())
+                    {
+                        queue.Formatter = new XmlMessageFormatter();
+
+                        // push message onto queue (inside of a transaction)
+                        trn.Begin();
+                        _messageNum++; // increment the message number
+                        queue.Send(payLoad, payLoad.UserName, trn);
+                        trn.Commit();
+
+                        Console.WriteLine("Message {0} queued", _messageNum);
+                    }
+                }
+                catch
+                {
+                    trn.Abort(); // rollback the transaction
+                }
+            }
+        }
+        private static void OnPeekCompleted(Object source, PeekCompletedEventArgs asyncResult)
+        {
+            // Connect to the queue.
+            MessageQueue mq = (MessageQueue)source;
+
+            // create transaction
+            using (var txn = new MessageQueueTransaction())
+            {
+                try
+                {
+                    // retrieve message and process
+                    txn.Begin();
+                  
+                    var message = mq.Receive(txn);
+
+                    if (message != null)
+                    {
+                        UserRequest.Add(message.Label);
+                    }
+
+                    // message will be removed on txn.Commit.
+                    txn.Commit();
+                }
+                catch (Exception ex)
+                {
+                    // on error don't remove message from queue
+                    Console.WriteLine(ex.ToString());
+                    txn.Abort();
+                }
+            }
+
+            // Restart the asynchronous peek operation.
+           // mq.BeginPeek();
+        }
+
+
+    }
+}
---
+++ Usp_getcustorders.sql
@@ -0,0 +1,70 @@
+﻿USE [Northwind]
+
+go 
+
+/****** Object:  StoredProcedure [dbo].[usp_GetCustOrders]    
+SET ansi_nulls ON 
+
+go 
+
+SET quoted_identifier ON 
+
+go 
+
+go 
+
+IF Object_id('dbo.usp_GetCustOrders', 'P') IS NOT NULL 
+  DROP PROCEDURE dbo.usp_getcustorders; 
+
+go 
+
+CREATE PROCEDURE [dbo].[Usp_getcustorders] @custid   AS NCHAR(5), 
+                                           @emplid   AS NCHAR(5), 
+                                           @fromdate AS DATETIME, 
+                                           @todate   AS DATETIME 
+AS 
+    SET nocount ON; 
+
+  BEGIN 
+      SELECT Concat(Employee.titleofcourtesy, ' ', Employee.firstname, 
+             ' ', Employee.lastname)                                    AS 
+             EmployeeFullName, 
+             [dbo].[shippers].[companyname]                             AS 
+             shippersCompanyName, 
+             [dbo].[customers].[companyname]                            AS 
+             CompanyName 
+             , 
+             orderdate, 
+             Sum(freight)                                               AS 
+             TotalFreighCost, 
+             Sum(CustD.unitprice * CustD.quantity - ( CustD.discount )) AS 
+             TotalOrderValue, 
+             Count(DISTINCT ProdDetails.productname)                    AS 
+             NumberOfDifferntProducts, 
+             Count([dbo].[orders].orderid)                              AS 
+             NumberOfOrder 
+      FROM   [dbo].[orders] 
+             LEFT OUTER JOIN [dbo].[customers] 
+                          ON [dbo].[orders].[customerid] = 
+                             [dbo].[customers].[customerid] 
+             LEFT OUTER JOIN [dbo].[employees] AS Employee 
+                          ON [dbo].[orders].[employeeid] = Employee.[employeeid] 
+             LEFT OUTER JOIN [dbo].[shippers] 
+                          ON [dbo].[orders].[shipvia] = 
+                             [dbo].[shippers].[shipperid] 
+             LEFT OUTER JOIN [dbo].[order details] AS CustD 
+                          ON [dbo].[orders].orderid = CustD.orderid 
+             LEFT OUTER JOIN [dbo].[products] AS ProdDetails 
+                          ON CustD.[productid] = ProdDetails.productid 
+      WHERE  orderdate BETWEEN CONVERT(VARCHAR, @fromdate, 13) AND 
+                                      CONVERT(VARCHAR, @todate, 13) 
+             AND [dbo].[orders].[customerid] = Isnull(@custid, 
+                                               [dbo].[orders].[customerid]) 
+             AND [dbo].[orders].[employeeid] = Isnull(@emplid, 
+                                               [dbo].[orders].[employeeid]) 
+      GROUP  BY [dbo].[shippers].[companyname], 
+                [dbo].[customers].[companyname], 
+                Concat(Employee.titleofcourtesy, ' ', Employee.firstname, ' ', 
+                Employee.lastname), 
+                orderdate 
+  END 
